b2987ad89a9459f93269ccf7d01bb4aa0bbe7068
edudole <mintanaeducation@gmail.com>
2026-09-07 13:05:42 +0700

    Add files via upload

---
+++ app-config.js
@@ -0,0 +1,7 @@
+(() => {
+  'use strict';
+
+  window.APP_CONFIG = Object.freeze({
+    API_URL: 'https://script.google.com/macros/s/AKfycbx-zeS_e_SpsQn9qLlfrriQ80pn-_t9Hv1ctLp8mGTUuAZvFBchCE54dyv_PZt1ke6A/exec'
+  });
+})();
